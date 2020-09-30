0d5968ff0a739990f1e65994f4417aa2fbba19eb
VallariAg <val.agl002@gmail.com>
2020-10-01 02:41:12 +0530

    add CRUD operations on database

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,23 +1,32 @@
 import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { ObjectType, Field } from "type-graphql";
 
+//schema
+@ObjectType()
 @Entity()
 export class Post {
 
+    @Field()
     @PrimaryKey()
     id!: number;
 
+    @Field(() => String)
     @Property({ type: "date" })
     createdAt = new Date();
 
+    @Field(() => String)
     @Property({ type: "date", onUpdate: () => new Date() })
     updatedAt = new Date();
 
+    @Field()
     @Property({ type: "text" })
     title!: string;
 
+    @Field()
     @Property({ type: "text", nullable: true })
     body: string;
 
+    @Field()
     @Property({ type: "text", nullable: true })
     description: string;
 
---
+++ src/resolvers/post.ts
@@ -0,0 +1,72 @@
+import { Resolver, Query, Ctx, Arg, Int, Mutation } from "type-graphql";
+import { MyContext } from "../types";
+import { Post } from "../entities/Post";
+
+@Resolver()
+export class PostResolver {
+    @Query(() => [Post])
+    posts(@Ctx() { em }: MyContext): Promise<Post[] | null> {
+        return em.find(Post, {});
+    }
+
+    @Query(() => Post, { nullable: true })
+    post(
+        @Arg('id', () => Int) id: number,
+        @Ctx() { em }: MyContext
+    ): Promise<Post | null> {
+        return em.findOne(Post, { id });
+    }
+
+    @Mutation(() => Post, { nullable: true })
+    async createPost(
+        @Arg('title') title: string,
+        @Arg('body', () => String, { nullable: true }) body: string,
+        @Arg('description', () => String, { nullable: true }) description: string,
+        @Ctx() { em }: MyContext
+    ): Promise<Post> {
+        const post = em.create(Post, { title, body, description })
+        await em.persistAndFlush(post)
+        return post;
+    }
+
+
+    @Mutation(() => Post, { nullable: true })
+    async updatePost(
+        @Arg('id') id: number,
+        @Arg('title', () => String, { nullable: true }) title: string,
+        @Arg('body', () => String, { nullable: true }) body: string,
+        @Arg('description', () => String, { nullable: true }) description: string,
+        @Ctx() { em }: MyContext
+    ): Promise<Post | null> {
+        const post = await em.findOne(Post, { id });
+        if (!post) {
+            return null;
+        }
+        if (typeof title != 'undefined') {
+            post.title = title;
+        }
+        if (typeof body != 'undefined') {
+            post.body = body;
+        }
+        if (typeof description != 'undefined') {
+            post.description = description;
+        }
+
+        await em.persistAndFlush(post)
+        return post;
+    }
+
+
+    @Mutation(() => Boolean)
+    async deletePost(
+        @Arg('id') id: number,
+        @Ctx() { em }: MyContext
+    ): Promise<boolean> {
+        try {
+            await em.nativeDelete(Post, { id });
+            return true;
+        } catch (e) {
+            return false;
+        }
+    }
+}
